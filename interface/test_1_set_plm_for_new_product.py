3912f59d2ed0dffe4c07852876c5ad9354758b50
赵吉宁 <zhaojining@Francis.local>
2020-08-18 10:16:41 +0800

    aaaaa

--- interface/test_1_set_plm_for_new_product.py
+++ interface/test_1_set_plm_for_new_product.py
@@ -14,7 +14,7 @@ class TestSetPlm:
     def test_0(self, chrome_config):
         driver = chrome_config['driver']
         driver.maximize_window()
-        driver.get("https://service-wbs320.newtamp.cn/")
+        driver.get("https://service-wbs321.newtamp.cn/")
         # 等待元素出现
         self.wait_element(driver, classname='el-input__inner')
         # 定位用户名输入框并输入手机号
