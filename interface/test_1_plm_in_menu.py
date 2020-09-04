c86584474a5d11e61375ba058e2ddce546e4bdd0
赵吉宁 <zhaojining@Francis.local>
2020-09-04 16:33:25 +0800

    修改运行文件

--- interface/test_1_plm_in_menu.py
+++ interface/test_1_plm_in_menu.py
@@ -80,8 +80,3 @@ class TestPlmMenu:
 
         finally:
             pass
-
-
-if __name__ == "__main__":
-    pytest.main(['-v', '--tb=line', '-m=smoke', '--junitxml=test-report.xml'])
-    # pytest.main(['-v', '-s', '-m=smoke'])
